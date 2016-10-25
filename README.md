36f98a0721bf0f49e293892548ba5ba3cf3ec4b4
HuangKeQing <huangkeqing@zhuojianchina.com>
2016-10-25 15:15:42 +0800

    script部分大致掌握

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # WeexOne
+
+###目前状态
+1. 列表的自动更新/.$set(index, item)【动态绑定】的尝试
+2. 列表的refresh/load
+3. for循环/if-else
+4. [数组操作](http://www.w3school.com.cn/jsref/jsref_obj_array.asp)
+
+###目标
+1. 导入并运行[One项目](https://github.com/dodola/WeexOne)
+2. 掌握界面编写
+
+###尚有疑问
+1. list-demo上添加button
+2. One项目js文件导入不可用
--- wxhelloworld/src/main/java/com/kerchin/wxhelloworld/ImageLoader.java
+++ wxhelloworld/src/main/java/com/kerchin/wxhelloworld/ImageLoader.java
@@ -2,9 +2,13 @@ package com.kerchin.wxhelloworld;
 
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+import com.taobao.weex.WXEnvironment;
 import com.taobao.weex.adapter.IWXImgLoaderAdapter;
 import com.taobao.weex.common.WXImageStrategy;
 import com.taobao.weex.dom.WXImageQuality;
+
+import java.util.Map;
 
 /**
  * Created by ucmed on 2016/10/24.
@@ -13,6 +17,6 @@ import com.taobao.weex.dom.WXImageQuality;
 public class ImageLoader implements IWXImgLoaderAdapter {
     @Override
     public void setImage(String url, ImageView view, WXImageQuality quality, WXImageStrategy strategy) {
-        //
+        Glide.with(WXEnvironment.getApplication()).load(url).into(view);
     }
 }
